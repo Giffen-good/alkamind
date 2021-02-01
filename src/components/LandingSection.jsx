ced1b839e97fe472553219f3207ae104b06898fe
alf2rock <alf@alkamind.com>
2021-02-01 15:31:24 -0500

    Commit 5

--- src/components/LandingSection.jsx
+++ src/components/LandingSection.jsx
@@ -3,7 +3,7 @@ class LandingSection extends React.Component {
   render () {
     return (
       <div className="landing-section container is-max-desktop p-5 pb-6">
-        <p>The Alkamind moment is when the franetic noise in you mind calms down and you are able to engage in a conversation where you feel that you have been heard. No presciption or guru power play, but a simple conversation that opens up a peaceful clarity. Of couse there is a world of experience on all sides of the conversation, but the sustainability and resiliance of the systems that we create will be based on the threads of interpersonal relationships between those that build and those that maintain with care.</p>
+        <p>The moment is when the franetic noise in you mind calms down and you are able to engage in a conversation where you feel that you have been heard. No presciption or guru power play, but a simple conversation that opens up a peaceful clarity. Of couse there is a world of experience on all sides of the conversation, but the sustainability and resiliance of the systems that we create will be based on the threads of interpersonal relationships between those that build and those that maintain with care.</p>
       </div>
     )
   }
