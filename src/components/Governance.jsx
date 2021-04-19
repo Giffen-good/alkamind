5dd8e3bfd223521d005a79bc3dc2c1e043542843
alf2rock <alf@alkamind.com>
2021-04-18 22:43:08 -0400

    April2021v2

--- src/components/Governance.jsx
+++ src/components/Governance.jsx
@@ -9,8 +9,7 @@ class Governance extends React.Component {
             <p>Alkamind engagements start by understanding the client within a developmental model of maturity. Every organism or organization evolved through generally distinct levels of maturity. When energy and skillful means are focused on the appropriate next steps, the journey is both fulfilling and rewarding.</p>
               <p>Every environment is subject to evolutionary dynamics that reveal the underlying mystery of the challenges being faced. So, the first step is to establish an authentic origin story to understand how we got here. Then we would extend that narrative into a reasonable trajectory of development as an orientation road map.</p>
               <p>Once we have our road map, we create a portfolio of initiates to propel us forward. This portfolio will be made up of projects or activities to essentially &quot;keep the system alive&quot;, integrated with actions that improve the &quot;health and viability of the system&quot;, to finally work on transformation initiates to find a &quot;new way of being&quot;. These conversations will uncover the value that will need to be created at the point of presence autopoietically.</p>
-              <p>Setting Direction - Managing the Progress - Resolving Conflict and Barriers are the foundation of the governance structure. 
-              </p>
+              <p>Setting Direction - Managing the Progress - Resolving Conflict and Barriers are the foundation of the governance structure.</p>
           </div>
         </div>
       </section>
